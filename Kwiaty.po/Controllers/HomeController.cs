a31e9f139f6ba34c80297ee66bc83a871415243b
Przemek <ememek96@gmail.com>
2018-01-18 20:49:22 +0100

    initial

--- Kwiaty.po/Controllers/HomeController.cs
+++ Kwiaty.po/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Kwiaty.po.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public string JustText()
+        {
+            return "JUST TEXT HERE!";
+        }
     }
 }
---
+++ Kwiaty.po/Models/ProduktModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kwiaty.po.Models
+{
+    public class ProduktModel
+    {
+    }
+}
